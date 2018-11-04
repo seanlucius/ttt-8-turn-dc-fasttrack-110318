653260adfbc04904cef1c261c5dfa0692a6cc0bf
Sean Lucius <seanlucius22@icloud.com>
2018-11-04 16:48:13 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  input = (user_input.to_i) - 1
+  return input 
+end 
+
+def move(board, index, character = "X")
+  board[index] = character
+end
