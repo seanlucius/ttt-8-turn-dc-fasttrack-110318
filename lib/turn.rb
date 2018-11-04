9a49b7e9de2df39e5a820b50146f65c506b5e728
Sean Lucius <seanlucius22@icloud.com>
2018-11-04 16:48:53 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,24 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board, index)
+  if (position_taken?(board, index))
+    false
+  elsif !(index.between?(0, 8))
+    false 
+  else 
+    true
+  end 
+end 
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    false 
+  elsif board[index] == "X" || "O"
+    true
+  end 
+end 
+
 def input_to_index(user_input)
   input = (user_input.to_i) - 1
   return input 
